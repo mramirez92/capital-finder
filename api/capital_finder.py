b6eb6266fe72df6aa0eea220938f650321368a2d
Monica <ramirez.92.monica@gmail.com>
2023-01-09 21:04:08 -0600

    function edit

--- api/capital_finder.py
+++ api/capital_finder.py
@@ -5,7 +5,6 @@ import requests
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        global message
         s = self.path
         url_components = parse.urlsplit(s)
         query_string_list = parse.parse_qsl(url_components.query)
@@ -25,12 +24,8 @@ class handler(BaseHTTPRequestHandler):
                 # country_info.append(capital)
             message = f"The capital of {country_info[1]} is {country_info[0]}."
 
-        else:
-            message = "Country not found"
-
-        if "capital" in dic:
+        elif "capital" in dic:
             url = "https://restcountries.com/v3.1/capital/"
-
             r = requests.get(url + dic["capital"])
             data = r.json()
             country = []
@@ -41,7 +36,7 @@ class handler(BaseHTTPRequestHandler):
                 country.append(name)
             message = f"{country[0]} is the capital of {country[1]} ."
         else:
-            message = "Capital not found"
+            message = "Correct input, please enter something valid."
 
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
