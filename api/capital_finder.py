d12af5de453ce6b5e3c19b3090bcc7851c1c72d4
Monica <ramirez.92.monica@gmail.com>
2023-01-09 20:24:46 -0600

    functions for capital added

--- api/capital_finder.py
+++ api/capital_finder.py
@@ -1,7 +1,3 @@
-# https://restcountries.com/v3.1/name/peru/capital/lima
-#
-# https://restcountries.com/v3.1/capital/lima
-
 from http.server import BaseHTTPRequestHandler
 from urllib import parse
 import requests
@@ -10,11 +6,13 @@ import requests
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
+        # global message
         s = self.path
         url_components = parse.urlsplit(s)
         query_string_list = parse.parse_qsl(url_components.query)
         dic = dict(query_string_list)
 
+        # https://capital-finder-2.vercel.app/api/capital_finder?country=chile
         if "country" in dic:
             url = "https://restcountries.com/v3.1/name/"
             r = requests.get(url + dic["country"])
@@ -26,13 +24,27 @@ class handler(BaseHTTPRequestHandler):
                 country_info.append(capital)
                 country_info.append(name)
                 # country_info.append(capital)
-            city_capital = f" {country_info[0]} is the capital of {country_info[1]}"
+            message = f"The capital of {country_info[1]} is {country_info[0]}."
 
         else:
-            city_capital = "City not found"
+            self.send_error(404, "Country not found.")
+
+        if "capital" in dic:
+            url = "https://restcountries.com/v3.1/capital/"
+            r = requests.get(url + dic["capital"])
+            data = r.json()
+            country = []
+            for country_data in data:
+                capital = country_data["capital"][0]
+                name = country_data["name"]["common"]
+                country.append(capital)
+                country.append(name)
+            message = f"{country[0]} is the capital of {country[1]} ."
+        else:
+            self.send_error(404, "Capital not found.")
 
         self.send_response(200)
         self.send_header('Content-type', 'text/plain')
         self.end_headers()
-        self.wfile.write(city_capital.encode())
+        self.wfile.write(message.encode())
         return
