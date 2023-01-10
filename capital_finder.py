a4363a08a7dba9851487a1e2f438f9d0173e1cde
Monica <ramirez.92.monica@gmail.com>
2023-01-09 18:51:14 -0600

    change to append

--- capital_finder.py
+++ capital_finder.py
@@ -15,15 +15,16 @@ class handler(BaseHTTPRequestHandler):
         query_string_list = parse.parse_qsl(url_components.query)
         dic = dict(query_string_list)
 
-        # city_name = rest_countries.get("name")
         if "name" in dic:
             url = "https://restcountries.com/v3.1/name/"
             r = requests.get(url + dic["name"])
             data = r.json()
             country_info = []
             for country_data in data:
-                name_capital = country_data["name"][0]["capital"][0]
-                country_info.append(name_capital)
+                name = country_data["name"][0]
+                capital = country_data["capital"][0]
+                country_info.append(name)
+                country_info.append(capital)
             city_capital = f"{country_info[1]} is the capital of {country_info[0]}"
         else:
             city_capital = "City not found"
